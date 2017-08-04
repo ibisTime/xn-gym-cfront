63c0ef5a0a7d69ef453eed908727c482bbb3da99
wulq <wlqsltz@163.com>
2017-08-04 15:25:28 +0800

    1、设置支付密码显示手机号 2、用户页面显示昵称 3、公告列表显示图标 4、首页活动和私教样式修改 5、私课订单确认页显示最多报名人数的提示

--- static/js/app/config.js
+++ static/js/app/config.js
@@ -1,24 +1,33 @@
 var SYSTEM_CODE = "CD-ZWZJ000012";
-var PIC_PREFIX = 'http://otomdv0mr.bkt.clouddn.com/';
+var PIC_PREFIX =
+  'http://otomdv0mr.bkt.clouddn.com/';
 
 (function() {
-    // 判断是否登录
-    if (!/\/redirect\.html/.test(location.href)) {
-        var arr,
-            reg = new RegExp("(^| )userId=([^;]*)(;|$)"),
-            userId,
-            userReferee = "";
-        if (arr = document.cookie.match(reg))
-            userId = unescape(arr[2]);
-        // 未登录
-        if (!userId) {
-            var reg = new RegExp("(^|&)userReferee=([^&]*)(&|$)", "i");
-            var r = window.location.search.substr(1).match(reg);
-            if (r != null)
-                userReferee = decodeURIComponent(r[2]);
-            sessionStorage.setItem("userReferee", userReferee);
-            sessionStorage.setItem("l-return", location.pathname + location.search);
-            location.replace("../user/redirect.html");
-        }
+  // 判断是否登录
+  if (!/\/redirect\.html/.test(location.href)) {
+    var arr,
+      reg = new RegExp(
+        "(^| )userId=([^;]*)(;|$)"),
+      userId,
+      userReferee = "";
+    if (arr = document.cookie.match(reg))
+      userId = unescape(arr[2]);
+
+    // 未登录
+    if (!userId) {
+      var reg = new RegExp(
+        "(^|&)userReferee=([^&]*)(&|$)(^|&)userReferee=([^&" +
+        "]*)(&|$)(^|&)userReferee=([^&]*)(&|$)",
+        "i");
+      var r = window.location.search.substr(1).match(
+        reg);
+      if (r != null)
+        userReferee = decodeURIComponent(r[2]);
+      sessionStorage.setItem("userReferee",
+        userReferee);
+      sessionStorage.setItem("l-return",
+        location.pathname + location.search);
+      location.replace("../user/redirect.html");
     }
+  }
 })()
--- static/js/app/controller/user/User.js
+++ static/js/app/controller/user/User.js
@@ -1,68 +1,79 @@
 define([
-    'app/controller/base',
-    'app/module/foot',
-    'app/interface/UserCtr',
-    'app/interface/AccountCtr'
+  'app/controller/base',
+  'app/module/foot',
+  'app/interface/UserCtr',
+  'app/interface/AccountCtr'
 ], function(base, Foot, UserCtr, AccountCtr) {
-    const SUFFIX = "?imageMogr2/auto-orient/thumbnail/!200x200r";
-    init();
+  const SUFFIX =
+    "?imageMogr2/auto-orient/thumbnail/!200x200r";
+  init();
 
-    function init() {
-        // $("#logout").click(function() {
-        //     base.clearSessionUser();
-        //     location.reload(true);
-        // });
-        Foot.addFoot(3);
-        base.showLoading("加载中...", 1);
-        $.when(
-            getUserInfo(),
-            getAccount()
-        ).then(base.hideLoading);
-        getUnfinishedOrders();
-    }
-    // 获取未完成的订单数量
-    function getUnfinishedOrders() {
-        UserCtr.getUnfinishedOrders()
-            .then((data) => {
-                if(data.activityCount) {
-                    $("#activityWrap")
-                        .append(`<span class="am-badge am-badge-not-a-wrapper">
+  function init() {
+    // $("#logout").click(function() {
+    //     base.clearSessionUser();
+    //     location.reload(true);
+    // });
+    Foot.addFoot(3);
+    base.showLoading("加载中...", 1);
+    $.when(
+      getUserInfo(),
+      getAccount()
+    ).then(base.hideLoading);
+    getUnfinishedOrders();
+  }
+  // 获取未完成的订单数量
+  function getUnfinishedOrders() {
+    UserCtr.getUnfinishedOrders()
+      .then((data) => {
+        if (data.activityCount) {
+          $("#activityWrap")
+            .append(
+              `<span class="am-badge am-badge-not-a-wrapper">
                             <sup class="am-badge-text">${data.activityCount}</sup>
-                        </span>`);
-                }
-                if(data.orgCourseCount) {
-                    $("#courseWrap")
-                        .append(`<span class="am-badge am-badge-not-a-wrapper">
+                        </span>`
+            );
+        }
+        if (data.orgCourseCount) {
+          $("#courseWrap")
+            .append(
+              `<span class="am-badge am-badge-not-a-wrapper">
                             <sup class="am-badge-text">${data.orgCourseCount}</sup>
-                        </span>`);
-                }
-                if(data.perCourseCount) {
-                    $("#coachWrap")
-                        .append(`<span class="am-badge am-badge-not-a-wrapper">
+                        </span>`
+            );
+        }
+        if (data.perCourseCount) {
+          $("#coachWrap")
+            .append(
+              `<span class="am-badge am-badge-not-a-wrapper">
                             <sup class="am-badge-text">${data.perCourseCount}</sup>
-                        </span>`);
-                }
-            });
-    }
-    // 获取账户信息
-    function getAccount() {
-        return AccountCtr.getAccount()
-            .then(function(data) {
-                data.forEach(function(d, i) {
-                    if (d.currency == "CNY") {
-                        $("#cnyAmount").text(base.formatMoney(d.amount));
-                    } else if (d.currency == "JF") {
-                        $("#jfAmount").text(base.formatMoney(d.amount));
-                    }
-                })
-            });
-    }
-    // 获取用户信息
-    function getUserInfo() {
-        return UserCtr.getUser().then(function(data) {
-            $("#nickName").text(data.nickname);
-            $("#userImg").attr("src", base.getAvatar(data.userExt.photo));
-            $("#mobile").text(data.mobile);
-        });
-    }
+                        </span>`
+            );
+        }
+      });
+  }
+  // 获取账户信息
+  function getAccount() {
+    return AccountCtr.getAccount()
+      .then(function(data) {
+        data.forEach(function(d, i) {
+          if (d.currency == "CNY") {
+            $("#cnyAmount").text(base.formatMoney(
+              d.amount));
+          } else if (d.currency == "JF") {
+            $("#jfAmount").text(base.formatMoney(
+              d.amount));
+          }
+        })
+      });
+  }
+  // 获取用户信息
+  function getUserInfo() {
+    return UserCtr.getUser().then(function(
+      data) {
+      $("#nickname").text(data.nickname);
+      $("#userImg").attr("src", base.getAvatar(
+        data.userExt.photo));
+      $("#mobile").text(data.mobile);
+    });
+  }
 });
